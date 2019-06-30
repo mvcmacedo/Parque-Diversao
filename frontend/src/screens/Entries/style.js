4a7e4670bb14545e0043949f24d22bef3afe023d
Marcus Macedo <marcus.macedom@gmail.com>
2019-06-30 20:34:51 -0300

    Init backend branch

--- frontend/src/screens/Entries/style.js
+++
@@ -1,6 +0,0 @@
-import Styled from 'styled-components';
-
-export const Container = Styled.div`
-  display: flex;
-  justify-content: center;
-`;
--- frontend/src/screens/Home/style.js
+++
@@ -1,8 +0,0 @@
-import Styled from 'styled-components';
-
-export const Container = Styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 3%;
-  background: #eee;
-`;
